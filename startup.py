c39ed7b68d8b3770c6c5e5beb85e37a31144e2e1
stirelshka8 <stirelshka8@gmail.com>
2023-05-17 13:45:43 +0300

    Правки и удаление лишнего #1

--- startup.py
+++ startup.py
@@ -1,5 +1,22 @@
 import os
+from sys import platform
+
+
+def exists_file(command: str):
+    if os.path.exists(".env"):
+        os.system(command)
+    else:
+        print("Отсутствует файл <.env>")
+
+
+def startup():
+    if platform == "linux" or platform == "linux2":
+        exists_file("python3 manage.py runserver")
+    elif platform == "darwin":
+        exists_file("python3 manage.py runserver")
+    elif platform == "win32":
+        exists_file("python manage.py runserver")
 
 
 if __name__ == '__main__':
-    os.system("python3 manage.py runserver")
+    startup()
